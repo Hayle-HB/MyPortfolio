e0d96b416079af9707bd3b79d8af419ef506fa9b
Haylemeskel Haylemariam <137502715+Hayle-HB@users.noreply.github.com>
2024-09-20 23:45:48 +0300

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const router = require("./models/testimonialsRouter");
+const router = require("./routers/testimonialsRouter");
+const mongoose = require("mongoose");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/api", router);
 
-app.listen(3000, () => {
-  console.log("Server running on port 5000");
-});
+// mongobd connection
+
+const url =
+  "mongodb+srv://hayle_hb:<password>@nodejs.b3kc1wk.mongodb.net/MyPortfolio?retryWrites=true&w=majority&appName=NodeJS";
+
+
+mongoose
+  .connect(url)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("MongoDB connected and Server running on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+  });
